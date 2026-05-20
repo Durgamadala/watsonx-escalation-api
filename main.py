bf7766d2f8ab3e3b15a59e21e22c2e9003f211de
Durgamadala <durga01.madala@gmail.com>
2026-05-20 15:09:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import random
+import smtplib
+from email.mime.text import MIMEText
 
 app = FastAPI(title="Escalation API", version="1.0.0")
 
@@ -9,6 +12,7 @@ class EscalationRequest(BaseModel):
     department: str
     issue: str
     status: str
+    email: str
 
 
 @app.get("/health")
@@ -18,9 +22,46 @@ def health():
 
 @app.post("/escalate")
 def escalate(data: EscalationRequest):
+
+    # Generate Dynamic Ticket ID
+    ticket_id = f"INC{random.randint(10000,99999)}"
+
+    # Email Body
+    body = f"""
+Hello {data.employee_name},
+
+Your issue has been escalated successfully.
+
+Ticket ID: {ticket_id}
+Department: {data.department}
+Issue: {data.issue}
+
+Our support team will reach out to you shortly.
+
+Regards,
+IT Support Team
+"""
+
+    # Your Gmail Credentials
+    sender_email = "durga01.madala@gmail.com"
+    sender_password = "iyhx lspm bhxo dlka"
+
+    # Create Email
+    msg = MIMEText(body)
+    msg["Subject"] = "Issue Escalation Confirmation"
+    msg["From"] = sender_email
+    msg["To"] = data.email
+
+    # Send Email
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender_email, sender_password)
+    server.sendmail(sender_email, data.email, msg.as_string())
+    server.quit()
+
     return {
         "status": "success",
-        "ticket_id": "INC12345",
-        "message": "Issue escalated successfully",
+        "ticket_id": ticket_id,
+        "message": "Issue escalated successfully and email sent",
         "received_data": data.dict()
     }
