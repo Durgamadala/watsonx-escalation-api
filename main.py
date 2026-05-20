b3095debafaf8a316ca487c8b4041971a6262655
Durgamadala <durga01.madala@gmail.com>
2026-05-20 11:47:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI(
-    title="Escalation API",
-    version="1.0.0",
-    openapi_version="3.0.0"
-)
+app = FastAPI(title="Escalation API", version="1.0.0")
+
 
 class EscalationRequest(BaseModel):
     employee_name: str
@@ -19,7 +16,7 @@ def health():
     return {"status": "ok"}
 
 
-@app.post("/escalate", operation_id="escalateIssue")
+@app.post("/escalate")
 def escalate(data: EscalationRequest):
     return {
         "status": "success",
